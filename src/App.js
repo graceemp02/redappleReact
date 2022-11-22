9a132fb2fcc643157ccd2a8f6818292492ffedb2
Anees Malik <grace.emp02@gmail.com>
2022-11-22 22:06:10 +0500

    Added Drawr

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import MiniDrawer from './components/MiniDrawer';
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+function App() {
+  return (
+    <div className="App">
+      <MiniDrawer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MiniDrawer.js
@@ -0,0 +1,197 @@
+import logo from "../assests/logo.png";
+import * as React from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MuiDrawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import PeopleAltRoundedIcon from "@mui/icons-material/PeopleAltRounded";
+import SettingsRoundedIcon from "@mui/icons-material/SettingsRounded";
+import TrendingUpRoundedIcon from "@mui/icons-material/TrendingUpRounded";
+import AddPhotoAlternateRoundedIcon from "@mui/icons-material/AddPhotoAlternateRounded";
+import ConstructionRoundedIcon from "@mui/icons-material/ConstructionRounded";
+import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import Grid from "@mui/material/Grid";
+const icons = [
+  <HomeRoundedIcon />,
+  <PeopleAltRoundedIcon />,
+  <SettingsRoundedIcon />,
+  <TrendingUpRoundedIcon />,
+  <AddPhotoAlternateRoundedIcon />,
+  <ConstructionRoundedIcon />,
+];
+const drawerWidth = 240;
+
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: "hidden",
+});
+
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}));
+
+export default function MiniDrawer() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+
+      <Drawer
+        variant="permanent"
+        open={open}
+        onMouseOver={handleDrawerOpen}
+        onMouseOut={handleDrawerClose}
+      >
+        <DrawerHeader>
+          <img
+            className="logo"
+            src={logo}
+            onClick={handleDrawerOpen}
+            alt="logo"
+          />
+          {open && (
+            <Typography
+              variant="h5"
+              sx={{
+                position: "fixed",
+                left: 75,
+                lineHeight: 1,
+              }}
+            >
+              Red <br />
+              Apple
+            </Typography>
+          )}
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {[
+            "Dashboard",
+            "Customers",
+            "Machines",
+            "Reportings",
+            "Advertisment",
+            "Installation",
+          ].map((text, index) => (
+            <ListItem key={text} disablePadding sx={{ display: "block" }}>
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? "initial" : "center",
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : "auto",
+                    justifyContent: "center",
+                  }}
+                >
+                  {icons[index]}
+                </ListItemIcon>
+                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+
+        <Divider />
+        <List sx={{ marginTop: "auto" }}>
+          <ListItem key={"text"} disablePadding sx={{ display: "block" }}>
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? "initial" : "center",
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
+                }}
+              >
+                <AccountCircleRoundedIcon />
+              </ListItemIcon>
+              <ListItemText primary="Name" sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Grid container spacing={1} columns={4}>
+          <Grid item xs={1}>
+            <Item>xs=8</Item>
+          </Grid>
+          <Grid item xs={1}>
+            <Item>xs=8</Item>
+          </Grid>
+          <Grid item xs={1}>
+            <Item>xs=8</Item>
+          </Grid>
+          <Grid item xs={1}>
+            <Item>xs=8</Item>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+  );
+}
