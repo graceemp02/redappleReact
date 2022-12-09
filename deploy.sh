9cf87334f21577572bba983840d5f2b12ba877aa
Anees Malik <grace.emp02@gmail.com>
2022-12-09 18:07:48 +0500

    alpha

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,23 @@
 
-echo 'Switching Branch Master....................'
+echo "Switching Branch Main........................................"
 git checkout main
 
-echo 'Adding to git....................'
+echo "Adding to git........................................"
 git add .
 
-echo 'commiting with message '$1
-git commit -m $1
+echo "Enter commiting Message........................................"
+read Msg
 
-echo 'git pushing to GitHub....................'
+echo "commiting with message\"$Msg\" ...................."
+git commit -m "$Msg"
+
+echo "git pushing to GitHub........................................"
 git push
 
-echo 'Building App....................'
+echo "Building App........................................"
 npm run build
 
-echo 'Deploying Files to Server....................'
+echo "Deploying Files to Server........................................"
 scp -r build/* root@146.190.65.198:/var/www/html/
 
-echo 'Done!!'
+echo "Done!! ........................................"
---
+++ test.sh
@@ -0,0 +1,3 @@
+echo "We have got $# no of messahes"
+read Person
+echo "Hello \"$Person"
